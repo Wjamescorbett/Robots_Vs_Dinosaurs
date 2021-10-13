37dcbae18b97370bc5a327deb421e1eb49b2e058
James Corbett <wjamescorbett@gmail.com>
2021-10-13 12:24:16 -0400

    Started weapon and robot classes

---
+++ robot.py
@@ -0,0 +1,9 @@
+class Robot:
+    def __init__(self):
+        self.name = ''
+        self.health = 100
+        self.weapon = Weapon()
+
+    def __init__(self, name):
+        self.attack(self, dinosaour):
+        
--- weapon.py
+++ weapon.py
@@ -1,1 +1,7 @@
-Class = Weapon():
+class Weapon:
+    def __init__(self):
+        self.name = ""
+        self.attack_power = 1
+
+    def __init__(self, name, attack_power):
+        
