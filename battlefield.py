3b252a655a72df5085174fab4baf474eb5e35c8f
James Corbett <wjamescorbett@gmail.com>
2021-10-14 12:12:12 -0400

    Made functions to Battlefield class

--- battlefield.py
+++ battlefield.py
@@ -1,6 +1,43 @@
 from fleet import Fleet
 from herd import Herd
 
+fleet = Fleet()
+fleet.robots()
+
+herd = Herd()
+herd.robots()
+
 class Battlefield:
     def __init__(self):
-        
+        self.fleet = fleet
+        self.herd = herd
+    def run_game(self):
+        self.display_welcome()
+
+    def display_welcome(self):
+        print("Welcome robots and dinosaurs! Prepare to fight!")
+        self.battle()
+
+    def battle(self):
+        print("Made it this far.")
+        dino_turn(herd.dinosaur)
+
+    def dino_turn(self, dinosaur):
+        dinosaur.attack(fleet.robot)
+        show_dino_opponent_options()
+
+    def robo_turn(self, robot):
+        robot.attack(herd.dinosaur)
+        show_robo_opponent_options()
+
+    def show_dino_opponent_options(self):
+
+    def show_robo_opponent_options(self):
+
+    def display_winners(self):
+        if herd.health == 0:
+            print("Robots win!")
+        if  fleet.health == 0:
+            print("Dinosaurs win!")
+        else:
+            battle()  
